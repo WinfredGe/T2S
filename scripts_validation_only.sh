e86d8a1752c7af967a0107422c26a9fb2270669a
Yunfeng <41022342+breakwa@users.noreply.github.com>
2025-03-19 10:40:02 +0800

    Update scripts_validation_only.sh
    Added decimal point

--- scripts_validation_only.sh
+++ scripts_validation_only.sh
@@ -1,6 +1,6 @@
-python evaluation.py --dataset_name 'exchangerate_24' --cfg_scale 7 --total_step 100
-python evaluation.py --dataset_name 'exchangerate_48' --cfg_scale 12 --total_step 60
-python evaluation.py --dataset_name 'exchangerate_96' --cfg_scale 5 --total_step 100
+python evaluation.py --dataset_name 'exchangerate_24' --cfg_scale 7.0 --total_step 100
+python evaluation.py --dataset_name 'exchangerate_48' --cfg_scale 12.0 --total_step 60
+python evaluation.py --dataset_name 'exchangerate_96' --cfg_scale 5.0 --total_step 100
 
 
 python evaluation.py --dataset_name 'electricity_24' --cfg_scale 5.0 --total_step 60

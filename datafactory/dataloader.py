2ddc7f6d19d9b88d199d4d8877162a09e0a49ef0
Yunfeng <41022342+WinfredGe@users.noreply.github.com>
2025-05-04 17:12:18 +0800

    Update dataloader.py

--- datafactory/dataloader.py
+++ datafactory/dataloader.py
@@ -44,71 +44,35 @@ data_dict = {
     'exchangerate_24': 'embedding_cleaned_exchangerate_24',
     'exchangerate_48': 'embedding_cleaned_exchangerate_48',
     'exchangerate_96': 'embedding_cleaned_exchangerate_96',
-    'nationalillness': 'embedding_cleaned_nationalillness',
-    'nationalillness_24': 'embedding_cleaned_nationalillness_24',
-    'nationalillness_48': 'embedding_cleaned_nationalillness_48',
-    'nationalillness_96': 'embedding_cleaned_nationalillness_96',
     'traffic': 'embedding_cleaned_traffic',
     'traffic_24': 'embedding_cleaned_traffic_24',
     'traffic_48': 'embedding_cleaned_traffic_48',
     'traffic_96': 'embedding_cleaned_traffic_96',
-    'weather': 'embedding_cleaned_weather',
-    'weather_24': 'embedding_cleaned_weather_24',
-    'weather_48': 'embedding_cleaned_weather_48',
-    'weather_96': 'embedding_cleaned_weather_96',
 
     'MMD-Agriculture': 'embedding_cleaned_Agriculture',
     'MMD-Climate': 'embedding_cleaned_Climate',
-    'MMD-Energy': 'embedding_cleaned_Energy',
-    'MMD-Health_AFR': 'embedding_cleaned_Health_AFR',
     'MMD-Health_US': 'embedding_cleaned_Health_US',
-    'MMD-Security': 'embedding_cleaned_Security',
     'MMD-Traffic': 'embedding_cleaned_Traffic',
     'MMD-Economy': 'embedding_cleaned_Economy',
-    'MMD-Environment': 'embedding_cleaned_Environment',
     'MMD-SocialGood': 'embedding_cleaned_SocialGood',
 
     'MMD-Agriculture_24': 'embedding_cleaned_Agriculture_24',
     'MMD-Climate_24': 'embedding_cleaned_Climate_24',
-    'MMD-Energy_24': 'embedding_cleaned_Energy_24',
-    'MMD-Health_AFR_24': 'embedding_cleaned_Health_AFR_24',
     'MMD-Health_US_24': 'embedding_cleaned_Health_US_24',
-    'MMD-Security_24': 'embedding_cleaned_Security_24',
     'MMD-Traffic_24': 'embedding_cleaned_Traffic_24',
     'MMD-Economy_24': 'embedding_cleaned_Economy_24',
-    'MMD-Environment_24': 'embedding_cleaned_Environment_24',
     'MMD-SocialGood_24': 'embedding_cleaned_SocialGood_24',
     'MMD-Agriculture_48': 'embedding_cleaned_Agriculture_48',
     'MMD-Climate_48': 'embedding_cleaned_Climate_48',
-    'MMD-Energy_48': 'embedding_cleaned_Energy_48',
-    'MMD-Health_AFR_48': 'embedding_cleaned_Health_AFR_48',
     'MMD-Health_US_48': 'embedding_cleaned_Health_US_48',
-    'MMD-Security_48': 'embedding_cleaned_Security_48',
-    'MMD-Traffic_48': 'embedding_cleaned_Traffic_48',
     'MMD-Economy_48': 'embedding_cleaned_Economy_48',
-    'MMD-Environment_48': 'embedding_cleaned_Environment_48',
     'MMD-SocialGood_48': 'embedding_cleaned_SocialGood_48',
     'MMD-Agriculture_96': 'embedding_cleaned_Agriculture_96',
     'MMD-Climate_96': 'embedding_cleaned_Climate_96',
-    'MMD-Energy_96': 'embedding_cleaned_Energy_96',
-    'MMD-Health_AFR_96': 'embedding_cleaned_Health_AFR_96',
     'MMD-Health_US_96': 'embedding_cleaned_Health_US_96',
-    'MMD-Security_96': 'embedding_cleaned_Security_96',
     'MMD-Traffic_96': 'embedding_cleaned_Traffic_96',
     'MMD-Economy_96': 'embedding_cleaned_Economy_96',
-    'MMD-Environment_96': 'embedding_cleaned_Environment_96',
     'MMD-SocialGood_96': 'embedding_cleaned_SocialGood_96',
-
-    'TSL-Agriculture': 'embedding_Cleaned_TSL_AgricultureandFood',
-    'TSL-Educational': 'embedding_Cleaned_TSL_EducationalandPublicServices',
-    'TSL-Energy': 'embedding_Cleaned_TSL_EnergyandResourceConsumption',
-    'TSL-Entertainment': 'embedding_Cleaned_TSL_Entertainment',
-    'TSL-Environment': 'embedding_Cleaned_TSL_EnvironmentalandClimate',
-    'TSL-Health': 'embedding_Cleaned_TSL_HealthandMedicalData',
-    'TSL-Sales': 'embedding_Cleaned_TSL_SalesandMarketTrends',
-    'TSL-Technological': 'embedding_Cleaned_TSL_TechnologicalandDigitalTrends',
-    'TSL-Transport': 'embedding_Cleaned_TSL_TransportandTrafficTrends',
-    'TSL-Wildlife': 'embedding_Cleaned_TSL_WildlifeandNature',
 
     'SUSHI': 'embedding_cleaned_SUSHI',
 }
@@ -124,8 +88,6 @@ def loader_provider(args, period):
             if args.dataset_name == 'SUSHI':
                 Data_path = r"./Data/SUSHI/"
                 dataset = T2SDataset(name=Data_name,data_root=Data_path, period=period)
-            elif args.dataset_name.split('-')[0] == 'TSL':
-                dataset = T2SDataset(name=Data_name,data_root=f'./Data/TSL/', period=period)
             elif args.dataset_name.split('-')[0] == 'MMD':
                 dataset1 = T2SDataset(name=Data_name+'_24',data_root=f'./Data/MMD/', period=period)
                 dataset2 = T2SDataset(name=Data_name+'_48',data_root=f'./Data/MMD/', period=period)
@@ -142,8 +104,6 @@ def loader_provider(args, period):
         else:
             if args.dataset_name == 'SUSHI':
                 dataset = T2SDataset(name=Data_name, data_root=f'./Data/SUSHI/', period=period)
-            elif args.dataset_name.split('-')[0] == 'TSL':
-                dataset = T2SDataset(name=Data_name, data_root=f'./Data/TSL/', period=period)
             elif args.dataset_name.split('-')[0] == 'MMD':
                 dataset = T2SDataset(name=Data_name, data_root=f'./Data/MMD/', period=period)
             else:
